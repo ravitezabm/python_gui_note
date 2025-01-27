5ba9e076a1b0049ecec0d3f906129fe93bf9e50f
ravitez <abhiramanrs200@gmail.com>
2025-01-27 13:20:13 +0530

    added add button

--- gui.py
+++ gui.py
@@ -1,9 +1,26 @@
 import FreeSimpleGUI as sg
+import functions
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo",key='todo')
 add_button = sg.Button("Add")
 
-window = sg.Window("My Simple Notes",layout=[[label],[input_box,add_button]],)
+window = sg.Window("My Simple Notes",
+                   layout=[[label],[input_box,add_button]],
+                   font=('Helvetica',14))
 
-window.read()
+while True:
+    event,values=window.read()
+    match event:
+        case 'Add':
+            todos=functions.get_file()
+            new_todo = values['todo'] +"\n"
+            todos.append(new_todo)
+            functions.write_file(todos)
+        case sg.WIN_CLOSED:
+            break
+
+
+    print(event)
+    print(values)
+window.close()
