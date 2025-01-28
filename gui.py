1c3b485acae6712c102631f6a3d01b254ed73b31
ravitez <abhiramanrs200@gmail.com>
2025-01-28 06:51:52 +0530

    added add button

--- gui.py
+++ gui.py
@@ -1,26 +1,43 @@
 import FreeSimpleGUI as sg
 import functions
 
-label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo",key='todo')
-add_button = sg.Button("Add")
 
-window = sg.Window("My Simple Notes",
-                   layout=[[label],[input_box,add_button]],
+label = sg.Text("My to-do")
+input_box = sg.InputText(tooltip="Enter To-do",key='todo')
+add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_file(),key='todos',
+                      enable_events=True,size=[45,20])
+edit_button = sg.Button('Edit')
+
+
+window = sg.Window('Daily Notes',
+                   layout=[[label],[input_box,add_button],
+                           [list_box,edit_button]],
                    font=('Helvetica',14))
 
 while True:
-    event,values=window.read()
+    event,values = window.read()
+    print(event)
+    print(values)
     match event:
         case 'Add':
-            todos=functions.get_file()
-            new_todo = values['todo'] +"\n"
-            todos.append(new_todo)
+            todos = functions.get_file()
+            todo_new = values['todo'] + "\n"
+            todos.append(todo_new)
             functions.write_file(todos)
+            window['todos'].update(todos)
+        case 'Edit':
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+            todos = functions.get_file()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_file(todos)
+            window['todos'].update(todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+
         case sg.WIN_CLOSED:
             break
 
-
-    print(event)
-    print(values)
 window.close()
